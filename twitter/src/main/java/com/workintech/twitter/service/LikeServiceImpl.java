59abd1540bf1504b17ad303aa4aecb91e92fa1e3
sinemsevimlikurt <sevimlikurtsinem@gmail.com>
2025-05-04 18:42:08 +0300

    entity done.

--- twitter/src/main/java/com/workintech/twitter/service/LikeServiceImpl.java
+++ twitter/src/main/java/com/workintech/twitter/service/LikeServiceImpl.java
@@ -1,13 +1,7 @@
 package com.workintech.twitter.service;
 
 import com.workintech.twitter.entity.Like;
-import com.workintech.twitter.entity.LikeId;
-import com.workintech.twitter.entity.Tweet;
-import com.workintech.twitter.entity.User;
-import com.workintech.twitter.repository.LikeRepository;
-import com.workintech.twitter.repository.TweetRepository;
-import com.workintech.twitter.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
